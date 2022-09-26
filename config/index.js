2c1fdfab86f52a0d5c1553c847ec7c645dbcb705
梧桐 <39265858+wutongvip@users.noreply.github.com>
2022-09-26 08:54:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -91,15 +91,15 @@ export const USER_CONFIG = {
     },
     {
       // 想要发送的人的名字
-      name: "宝宝",
+      name: "宝子",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "owbZI63Yjjw1Wzj0lCz4YHtZCf-A",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "QPuHg2HbEDYSm3IDvfn2jqVv0bACzNRGgd8in8g36qU",
       // 所在省份
-      province: "河南",
+      province: "北京",
       // 所在城市
-      city: "平顶山",
+      city: "大兴",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '9-5',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
